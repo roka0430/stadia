33ff9d934cca8a286fb549dea6b9f737fea29dd3
Roka0430 <noxx0430@gmail.com>
2026-08-01 08:37:14 +0900

    feat: add overlay controls

--- public/script.js
+++ public/script.js
@@ -1,5 +1,23 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("body", () => ({
     overlayId: null,
+
+    overlayCloseHandler: {
+      overlay1: () => {
+        return true;
+      },
+      overlay2: () => {
+        return true;
+      },
+      overlay3: () => {
+        return true;
+      },
+    },
+
+    closeOverlay() {
+      const handler = this.overlayCloseHandler[this.overlayId];
+      if (handler && handler() === false) return;
+      this.overlayId = null;
+    },
   }));
 });
