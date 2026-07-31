b79b7201dc06ad1fc5ee05eb4c9be11daab385c7
Roka0430 <noxx0430@gmail.com>
2026-08-01 07:55:19 +0900

    fix: express.static path

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(express.static("/public"));
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
