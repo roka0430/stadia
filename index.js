19fcd0de7e2548d70576d2aaeffa3cc36e34958c
Roka0430 <noxx0430@gmail.com>
2026-08-01 08:13:00 +0900

    refactor: minimize index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import express from "express";
-import path from "path";
 
 const app = express();
 const PORT = 3000;
-
-const __filename = import.meta.filename;
-const __dirname = import.meta.dirname;
 
 app.use(express.json());
 app.use(express.static("public"));
