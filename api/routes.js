a587b446200b14639a14a9df6ef560d914fd9a59
Roka0430 <noxx0430@gmail.com>
2026-08-12 20:25:29 +0900

    feat: add subjects api

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import periods from "./periods.js";
+import subjects from "./subjects.js";
 
 const router = express.Router();
 
 router.use("/periods", periods);
+router.use("/subjects", subjects);
 
 export default router;
