67671f46c6adac0b164c5e7909e998b04c0b4455
Roka0430 <noxx0430@gmail.com>
2026-08-26 09:25:05 +0900

    feat: add select category feature

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,5 +1,40 @@
 document.addEventListener("alpine:init", () => {
+  Alpine.data("body", () => ({
+    categories: [],
+    currentCategory: null,
+
+    async init() {
+      this.categories = await this.loadCategories();
+      this.currentCategory = await this.loadCategory(1);
+    },
+
+    async loadCategories() {
+      const res = await fetch("/api/category");
+
+      if (!res.ok) {
+        throw new Error("failed to load categories.");
+      }
+
+      return await res.json();
+    },
+
+    async loadCategory(categoryId) {
+      const res = await fetch(`/api/category/${categoryId}`);
+
+      if (!res.ok) {
+        throw new Error("failed to load category.");
+      }
+
+      return await res.json();
+    },
+  }));
+
   Alpine.data("category", () => ({
     isOpen: false,
+
+    async selectCategory(categoryId) {
+      this.currentCategory = await this.loadCategory(categoryId);
+      this.isOpen = false;
+    },
   }));
 });
