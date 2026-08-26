6e3bc4a8077b47a7dfcb9a26f6d8ca11d6d4937c
Roka0430 <noxx0430@gmail.com>
2026-08-26 09:16:39 +0900

    feat: import category api

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import categoryApi from "./api/category.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api/category", categoryApi);
 
 app.listen(PORT);
