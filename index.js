54d98779e893c0fd787231f1e8daac5d9a51c05d
Roka0430 <noxx0430@gmail.com>
2026-09-11 16:02:40 +0900

    feat: update server port from 3000 to 3003

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import categoryApi from "./api/category.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3003;
 
 app.use(express.json());
 app.use(express.static("public"));
